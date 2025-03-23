fc951d81b7c6ec014aa43645da39390a24de2dac
Robert Pack <robstar.pack@gmail.com>
2025-03-23 19:01:05 +0100

    feat: select credetial name from available credentials

--- app/src/types.ts
+++ app/src/types.ts
@@ -1,9 +1,11 @@
 import {
-    TreeItemProps,
-    InputProps,
-    RadioGroupProps,
-    CheckboxProps,
-    TabListProps,
+  TreeItemProps,
+  InputProps,
+  RadioGroupProps,
+  CheckboxProps,
+  TabListProps,
+  SelectProps,
+  DropdownProps,
 } from "@fluentui/react-components";
 
 export type TreeItemOnChange = NonNullable<TreeItemProps["onOpenChange"]>;
@@ -11,3 +13,5 @@ export type InputChange = NonNullable<InputProps["onChange"]>;
 export type RadioChange = NonNullable<RadioGroupProps["onChange"]>;
 export type CheckboxChange = NonNullable<CheckboxProps["onChange"]>;
 export type TabSelect = NonNullable<TabListProps["onTabSelect"]>;
+export type SelectChange = NonNullable<SelectProps["onChange"]>;
+export type DropdownSelect = NonNullable<DropdownProps["onOptionSelect"]>;
